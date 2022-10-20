23f59e06426393a4bbb3d9785787eb9bfbc16497
Damanjot Singh <damanjotsingh1712@gmail.com>
2022-10-20 15:02:19 -0400

    update

--- server.js
+++ server.js
@@ -33,12 +33,14 @@ app.get("/",(req,res)=>{
     resText += "<p>Student Number: <mark><b> 148285216 </b> </mark> </p>";
     
     resText += `<p> <a href = "/CPA">CPA Students </a></p>
-                <p> <a href = "/highGPA"> highest GPA </a></p>
-                <p> <a href = "/allStudents"> All Students</a></p>
-                <p> <a href = "/addStudent"> Add a New Student</a></p>`
-    resText += "Note: Locate Specific student by Student ID, e.g., </p>";
-    resText += "http://localhost:8080/student/3 </p>";
+                <p> <a href = "/highGPA"> highest GPA </a></p>`
+    
     res.send(resText);
+});
+
+
+app.get('/home', (req, res)=>{
+    res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 app.get("/CPA", (req,res)=>{
